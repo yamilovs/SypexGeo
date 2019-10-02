ab39a20580914e6e346b9773392e16033b5569b3
Yamilov Stepan <yamilovs@gmail.com>
2019-10-03 00:28:48 +0500

    Added mode constants

---
+++ src/Mode.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yamilovs\SypexGeo;
+
+class Mode
+{
+    /**
+     * Work with the base file, default mode
+     *
+     * @var int
+     */
+    public const FILE = 0;
+
+    /**
+     * Batch processing, increases the speed when processing multiple IP at a time
+     *
+     * @var int
+     */
+    public const MEMORY = 1;
+
+    /**
+     * Database cached in memory, still increases batch processing speed, but requires more memory to load the entire database into memory
+     *
+     * @var int
+     */
+    public const BATCH = 2;
+}
