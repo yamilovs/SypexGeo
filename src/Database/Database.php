5a4dbd59374528a694360c646d5e0ce60c5757aa
Yamilov Stepan <yamilovs@gmail.com>
2019-10-03 16:21:33 +0500

    Added new database file that handle all operations with db

---
+++ src/Database/Database.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yamilovs\SypexGeo\Database;
+
+use Yamilovs\SypexGeo\Database\Exception\UnsupportedModeException;
+use Yamilovs\SypexGeo\Mode;
+
+class Database
+{
+    /**
+     * @var Reader
+     */
+    protected $reader;
+
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    public function __construct(Reader $reader, Config $config)
+    {
+        $this->reader = $reader;
+        $this->config = $config;
+    }
+
+    public function init(int $mode = Mode::FILE): void
+    {
+        switch ($mode) {
+            case Mode::FILE:
+                break;
+            case Mode::MEMORY:
+                // todo this
+                break;
+            case Mode::BATCH:
+                // todo this too
+                break;
+            default:
+                throw new UnsupportedModeException($mode);
+        }
+    }
+}
---
+++ src/Database/Exception/UnsupportedModeException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yamilovs\SypexGeo\Database\Exception;
+
+use InvalidArgumentException;
+use Yamilovs\SypexGeo\Mode;
+
+class UnsupportedModeException extends InvalidArgumentException
+{
+    public function __construct(int $mode)
+    {
+        $supported = implode('; ', array_map(function (string $key, int $val): string {
+            return sprintf('%s: %d', $key, $val);
+        }, array_keys(Mode::getModes()), Mode::getModes()));
+
+        parent::__construct(sprintf('Mode "%d" is not supported. Supported modes: %s', $mode, $supported), 406);
+    }
+}
