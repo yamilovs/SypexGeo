92ac31f1a3aab3f7b48e97e80c34e274fbf2c89d
Yamilov Stepan <yamilovs@gmail.com>
2019-10-07 16:02:23 +0500

    First version of unpack test. Added more cases indataProvider values in future

--- tests/Database/Processor/AbstractProcessorTest.php
+++ tests/Database/Processor/AbstractProcessorTest.php
@@ -9,6 +9,7 @@ use PHPUnit\Framework\TestCase;
 use Yamilovs\SypexGeo\City;
 use Yamilovs\SypexGeo\Country;
 use Yamilovs\SypexGeo\Database\Config;
+use Yamilovs\SypexGeo\Database\PackFormat;
 use Yamilovs\SypexGeo\Database\Processor\AbstractProcessor;
 use Yamilovs\SypexGeo\Database\Processor\ProcessorInterface;
 use Yamilovs\SypexGeo\Database\Reader;
@@ -47,6 +48,11 @@ class AbstractProcessorTest extends TestCase
             {
                 parent::searchPos($database, $ip, $min, $max);
             }
+
+            public function unpack(int $packFormat, ?string $item = null): array
+            {
+                return parent::unpack($packFormat, $item);
+            }
         };
     }
 
@@ -56,5 +62,26 @@ class AbstractProcessorTest extends TestCase
 
         $this->expectException(InvalidIpException::class);
         $processor->validateIp('fat_cat');
+    }
+
+    public function testUnpack(): void
+    {
+        $processor = $this->createProcessor();
+        $this->config->packFormats = [
+            PackFormat::COUNTRY => 'T:id/c2:iso/n2:lat/n2:lon/b:name_ru/b:name_en',
+        ];
+
+        [$id, $iso, $lat, $lon, $nameRu, $nameEn] = [5, 'RU', 111, 222, 'Имя', 'Name'];
+        $pack = pack('Ca2ssa7a7', $id, $iso, $lat, $lon, $nameRu, $nameEn);
+
+        $result = $processor->unpack(PackFormat::COUNTRY, $pack);
+
+        $this->assertIsArray($result);
+        $this->assertEquals($id, $result['id']);
+        $this->assertEquals($iso, $result['iso']);
+        $this->assertEquals((float)substr_replace((string)$lat, '.', 1, 0), $result['lat']);
+        $this->assertEquals((float)substr_replace((string)$lon, '.', 1, 0), $result['lon']);
+        $this->assertEquals($nameRu, $result['name_ru']);
+        $this->assertEquals($nameEn, $result['name_en']);
     }
 }
